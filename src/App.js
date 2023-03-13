9941a230870432b9d6582634579c5b89b186e99b
unknown <shivani@cronj.com>
2023-03-13 16:17:22 +0530

    Updates

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Routing from "./routes/Routes";
-import "bootstrap/dist/css/bootstrap.min.css";
-const App = () => {
-  return <Routing />;
-};
-
-export default App;
