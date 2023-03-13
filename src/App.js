4f2e6adc24ea6f8c6cd9167a674a9157fd3f9051
unknown <shivani@cronj.com>
2023-03-13 17:03:27 +0530

    chnages done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Routing from "./routes/Routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 const App = () => {
-  return <Routing />;
+  return <Routing userDetails={userDetails} />;
 };
 
 export default App;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,15 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 
 import Trello from "../containers/Trello";
 import Login from "../containers/login";
 import Register from "../containers/Register";
+const PrivateRoutes = () => {
+  let userid = localStorage.getItem("UserDetails") == null ? false : true;
+  return <>{userid ? <Outlet /> : <Navigate to="/" />};</>;
+};
+
 const Routing = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/board" element={<Trello />} />
-
+        <Route exact element={<PrivateRoutes />}>
+          <Route exact path="/board" element={<Trello />} />
+        </Route>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
